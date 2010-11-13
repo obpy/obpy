5b32690c56dfea7ce911d89ddde48a25014e93fc
openblocks <openblocks@users.sourceforge.net>
2010-11-13 16:39:40 -0600

    setup.py depends upon the third-party setuptools for installation. It's not installed by default, so I downloaded the setuptools installer, and coded setup.py to automatically install setuptools if it's not installed already!

--- src/setup.py
+++ src/setup.py
@@ -2,9 +2,53 @@ __author__="alexander"
 __date__ ="$Aug 4, 2010 2:56:12 PM$"
 
 import os
+import sys
 import glob
+import sys
 
-from setuptools import setup, find_packages
+try:
+    from setuptools import find_packages, setup
+except:
+
+    import tkMessageBox
+
+    tkMessageBox.showinfo('setuptools Installer', '''
+    The setuptools module was not found.
+    It will be installed now.
+
+    Note that you must have administrator permissions to continue!
+    ''')
+
+    retval = None
+
+    if os.name != 'nt':
+        retval = os.system('sh tpapps/setuptools-0.6c11-py2.6.egg')
+
+    else:
+        retval = os.system('tpapps/setuptools-0.6c11.win32-py2.6.exe')
+
+    if retval == 0:
+
+        tkMessageBox.showinfo('setuptools Installer', '''
+        The installation completed sucessfully!
+        ''')
+
+        if os.name != 'nt':
+            sys.exit(os.system('python setup.py install'))
+
+        else:
+            sys.exit(os.system('python.exe setup.py install'))
+
+    else:
+        tkMessageBox.showinfo('setuptools Installer', '''
+        There was a problem with the installation.
+        You probably did not have administrator permissions.
+
+        Otherwise, contact the OpenBlox developers at:
+        http://openblox.sourceforge.net/?q=forum for support.
+        ''')
+
+        sys.exit(1)
 
 hdp = {'nt' : '~', 'posix' : '.'}
 home = os.environ['HOME']
@@ -15,7 +59,16 @@ lua_dir = os.path.join(ob_dir, 'lualibs')
 
 data_files = glob.glob(os.path.join('data', '*'))
 
-setup (
+def postinstall():
+    
+    print 'Running postinstall...'
+
+    if os.name == 'posix':
+
+        print 'Changing permissions for config directory(%s)' % ob_dir
+        os.system('chmod -R 777 %s' % ob_dir)
+
+setup(
   name = 'OpenBlox Game Engine',
   version = '0.1',
 
@@ -28,7 +81,7 @@ setup (
   author = 'The OpenBlox Team',
   author_email = 'openblocks@users.sourceforge.net',
 
-  summary = 'Powerful yet simple 3D development framework',
+  description = 'Powerful yet simple 3D development framework',
   url = 'http://openblox.sourceforge.net',
   license = 'GNU GPL v3',
   long_description = """
@@ -39,3 +92,7 @@ setup (
   * A simple game distribution system
   """,
 )
+
+if 'install' in sys.argv:
+
+    postinstall()
