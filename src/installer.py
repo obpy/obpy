7f659b0c5b34359bd1d9793544831316f3ed73ac
openblocks <openblocks@users.sourceforge.net>
2011-01-27 06:56:20 -0600

    Modified installer.py to correct for subprocess.Popen string format error.

--- src/installer.py
+++ src/installer.py
@@ -85,7 +85,7 @@ def run_install():
             elif os.name == 'nt':
 
                 # Again, create and set
-                p = subprocess.Popen(shlex.split('C:\Python26\python.exe setup.py install'), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                p = subprocess.Popen(shlex.split('C:\Python26\python.exe setup.py install'), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, posix=False)
 
                 set_pipe(p)
 
