112d7b1465a788712f437f4e45261ebfde56652e
openblocks <openblocks@users.sourceforge.net>
2011-04-14 02:22:53 -0500

    * obpack.py works now - validates a game, and then compresses it

--- src/tools/obpack.py
+++ src/tools/obpack.py
@@ -26,6 +26,8 @@ __date__ ="$Mar 29, 2011 2:39:03 PM$"
 import os
 
 import zipfile
+
+import xml
 import xml.etree.ElementTree as xmlparser
 
 import wx
@@ -37,22 +39,65 @@ See http://openblox.tuxfamily.org/DirectoryLayout on how to add one.
 """
 
 invalid_xml = """
-Your world.xml file contains erroneous XML markup.
+Your world.xml file contains erroneous XML markup at %s.
 See http://openblox.tuxfamily.org/GameDevelopersManual for more info on this.
 """
 
 class GamePacker(object):
 
     def __init__(self):
+
         self.setup_dialog()
+        self.pack_game()
 
     def setup_dialog(self):
         
         initial_dir = os.getcwd()
         self.dir_dialog = wx.DirDialog(None, 'Select game directory', initial_dir)
 
+    def pack_game(self):
+
         self.dir_dialog.ShowModal()
-        self.validate_game(self.dir_dialog.GetPath())
+
+        directory = self.dir_dialog.GetPath()
+
+        if self.validate_game(directory) == True:
+
+            dest = self.get_output_zip()
+            directory = os.path.basename(directory)
+            self.write_game(directory, directory, dest)
+
+    def write_game(self, name, directory, dest, archive = None):
+
+         if archive is None:
+            archive = zipfile.ZipFile(dest, 'w')
+
+         paths = os.listdir(directory)
+
+         for p in paths:
+
+             if os.path.isdir(p):
+                self.write_game(p, os.path.join(directory, dest), archive)
+
+             else:
+
+                 if directory == name:
+                    archive.write(os.path.join(directory, p), arcname=p)
+
+
+                 else:
+                      archive.write(os.path.join(directory, p))
+
+    def get_output_zip(self):
+
+        style = wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT
+        initial_dir = os.getcwd()
+        filetype = 'OpenBlox games (*.zip)|*.zip'
+
+        dialog = wx.FileDialog(None, 'Select packed game filename', initial_dir, wildcard = filetype, style = style)
+        dialog.ShowModal()
+
+        return dialog.GetPath()
 
     def validate_game(self, game_dir):
 
@@ -64,6 +109,19 @@ class GamePacker(object):
 
             wx.MessageDialog(None, missing_world_xml % game_dir, dialog_caption, style).ShowModal()
             return False
+
+        try:
+            xmlparser.parse(open(os.path.join(game_dir, 'world.xml')))
+
+        except xml.parsers.expat.ExpatError, exception:
+
+            style = wx.ICON_ERROR | wx.OK
+            location = exception.args[0].split(':')[1][1:]
+
+            wx.MessageDialog(None, invalid_xml % location, dialog_caption, style).ShowModal()
+            return False
+
+        return True
 
 
 app = wx.App(False)
