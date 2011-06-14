07b79b8c23cc75869405e088a826486c52672c77
openblocks <openblocks@users.sourceforge.net>
2011-06-14 13:07:10 -0500

    * Removed accidentally copied doctest src/tools/gui.txt * New module (obengine.gui.menu) - implements a menu for OpenBlox
    Yes, the new module is a stub, but there's something special about this commit: Counting every single line of Python code (comments, actual code, etc..), and including bundled 3rd-party libraries (namely, lupa and copperode) , OpenBlox is exactly 11,500 lines. Pretty cool.

---
+++ src/obengine/gui/menu.py
@@ -0,0 +1,33 @@
+#
+# This module implements a simple me
+# See <TODO: No Sphinx docs yet - add some> for the primary source of documentation
+# for this module.
+#
+# Copyright (C) 2011 The OpenBlox Project
+#
+# This file is part of The OpenBlox Game Engine.
+#
+#     The OpenBlox Game Engine is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     The OpenBlox Game Engine is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with The OpenBlox Game Engine.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+__author__ = "openblocks"
+__date__  = "$Jun 14, 2011 11:16:52 AM$"
+
+
+import obengine.datatypes
+import obengine.event
+import obengine.depman
+
+obengine.depman.gendeps()
