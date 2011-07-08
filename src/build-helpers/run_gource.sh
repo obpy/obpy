4db6d0d5dc45cff2592e45f0fd91a27a2410c872
openblocks <openblocks@users.sourceforge.net>
2011-07-07 23:01:09 -0500

    * New build-helper (run_gource.sh) - runs Gource (http://code.google.com/p/gource) on OpenBlox.    Currently, it's a BASH shell script, but it'll probably be converted to Python in the future.

---
+++ src/build-helpers/run_gource.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Shell script to run Gource with OpenBlox
+# Author: openblocks
+# License: GNU GPLv3 or later
+
+
+GOURCE_FLAGS="--hide filenames,dirnames,usernames \
+--logo src/doc/source/_static/oblogo.png \
+--title OpenBlox --key -s 0.2 -a 2.0"
+
+GOURCE="gource"
+
+
+function usage()
+{
+    cat << EOF
+    usage: $0
+    This script runs Gource (http://code.google.com/p/gource/) over your local
+    copy of the OpenBlox Mercurial repository, producing a nice visualization
+    of OpenBlox's development of to the current commit in your local
+    repository clone. Mostly useless, but fun nonetheless :)
+
+    Environment variables:
+
+    * GOURCE_FLAGS: Flags/options to pass to Gource (currently $GOURCE_FLAGS)
+    * GOURCE: The path to the Gource executable (currently $GOURCE)
+EOF
+
+}
+
+
+function run_gource()
+{
+    cd ../..
+    echo $GOURCE $GOURCE_FLAGS
+
+    $GOURCE $GOURCE_FLAGS
+}
+
+# We're currently in the "build-helpers" subdirectory, and its parent directory
+# contains the Mercurial repository, so change directories
+
+if [ "--help" == "$1" ]; then
+
+    usage
+    exit 1
+
+fi
+
+if [ "-h" == "$1" ]; then
+
+    usage
+    exit 1
+
+fi
+
+run_gource
+
+exit 0
