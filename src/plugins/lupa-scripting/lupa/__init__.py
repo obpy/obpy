62e25f8e080ad1ad4fb13eb788a941338684626c
openblocks <openblocks@users.sourceforge.net>
2011-07-16 21:32:16 -0500

    * Ticket #18 fixed (thanks to Bednesti)

--- src/plugins/lupa-scripting/lupa/__init__.py
+++ src/plugins/lupa-scripting/lupa/__init__.py
@@ -1,20 +1,19 @@
-
 # We need to enable global symbol visibility for lupa in order to
 # support binary module loading in Lua.  If we can enable it here, we
 # do it temporarily.
 
-def _try_import_with_global_library_symbols():
-    try:
-        import DLFCN
-        dlopen_flags = DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL
-    except ImportError:
-        import ctypes
-        dlopen_flags = ctypes.RTLD_GLOBAL
+import sys
 
+# Panda3D hack for errant Windows sys.path
+if sys.platform == 'win32':
+    sys.path.insert(0, 'C:\\Program Files\\OpenBlox\\obengine\\scripting')
+
+def _try_import_with_global_library_symbols():
+    import DLFCN
     import sys
     old_flags = sys.getdlopenflags()
     try:
-        sys.setdlopenflags(dlopen_flags)
+        sys.setdlopenflags(DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL)
         import lupa._lupa
     finally:
         sys.setdlopenflags(old_flags)
@@ -28,9 +27,4 @@ del _try_import_with_global_library_symbols
 
 # the following is all that should stay in the namespace:
 
-from lupa._lupa import *
-
-try:
-    from lupa.version import __version__
-except ImportError:
-    pass
+from _lupa import *
