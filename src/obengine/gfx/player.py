d10b377e24fd2fa8e41a44e8aa3a2ccc25422cdb
openblocks <openblocks@users.sourceforge.net>
2011-07-20 11:20:08 -0500

    * Putting the finishing touches on obengine.gfx.player

--- src/obengine/gfx/player.py
+++ src/obengine/gfx/player.py
@@ -26,6 +26,9 @@ __author__ = "openblocks"
 __date__  = "$Oct 25, 2010 9:57:22 PM$"
 
 
+import obengine.datatypes
+import obengine.math
+import obengine.async
 import obengine.plugin
 import obengine.depman
 
@@ -33,7 +36,10 @@ obengine.depman.gendeps()
 
 
 def init():
+
     obengine.plugin.require('core.hardware')
+    obengine.plugin.require('core.graphics')
+    obengine.plugin.require('core.physics')
 
 
 class PlayerController(object):
@@ -41,8 +47,12 @@ class PlayerController(object):
     LINEAR_SPEED = 5
     ROT_SPEED = 2
 
-    def __init__(self, view):
+    def __init__(self, model, view):
+
+        self._model = model
         self._view = view
+
+        self._model.on_joined += lambda _: self._view.show()
 
     def forward(self):
         self._view.linear_velocity.x = self.LINEAR_SPEED
@@ -115,3 +125,51 @@ class KeyboardPlayerController(PlayerController):
         obplugin.core.hardware.KeyEvent.TYPE_UP)
         right_stop += self.rotation_stop
 
+
+class PlayerView(object):
+
+    def __init__(self, window):
+
+        import obplugin.core.graphics
+        import obplugin.core.physics
+
+        self._window = window
+        self._scheduler = self._window.scheduler
+        self._model = obplugin.core.graphics.Model('avatar', self._window)
+        self._model.load()
+
+        while self._model.load_okay is False:
+            self._scheduler.step()
+
+        self._capsule = obplugin.core.physics.CharacterCapsule()
+        self._scheduler.add(obengine.async.Task(self._update, priority = 5))
+        self.linear_velocity = obengine.math.Vector()
+
+    def show(self):
+
+        self._model.show()
+        self.phys_rep.enable()
+
+    def hide(self):
+
+        self._model.hide()
+        self._phys_rep.disable()
+
+    @obengine.datatypes.nested_property
+    def position():
+
+        def fget(self):
+            return self._capsule.position
+
+        def fset(self, new_pos):
+            self._capsule.position = new_pos
+
+        return locals()
+
+    def _update(self, task):
+        
+        self._capsule.linear_velocity = self.linear_velocity
+        self._model.position = self._capsule.position
+        self._model.rotation = self._capsule.rotation
+        
+        return task.AGAIN
