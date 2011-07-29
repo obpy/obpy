4870931682fb34954ad924e5ec3e44d8fe24d87a
openblocks <openblocks@users.sourceforge.net>
2011-07-29 15:06:02 -0500

    * Modified startlauncher.bat so as not to echo all its comments to stdout

--- src/startlauncher.bat
+++ src/startlauncher.bat
@@ -1,9 +1,10 @@
+@ECHO off
+
 :: Windows launcher for OpenBlox - it does nothing special,
 :: just start oblaunchgui.py.
 :: The only thing that relieves the user of any work is the need to call
 :: ppython.exe, which is Panda3D's special version of Python on Windows.
 :: Other than that, nothing tricky is done.
 
-@ECHO off
-ppython.exe oblaunchgui.py
+ppython.exe tools\oblaunchgui.py
 @ECHO on
