449fc1985056598a8764b76827d1ab5f7f3e5f08
openblocks <openblocks@localhost>
2011-12-01 23:45:47 -0600

    Changed default sunlight angle when creating a new BloxWorks project to something more realistic

--- src/tools/bloxworks/bloxworks/project.py
+++ src/tools/bloxworks/bloxworks/project.py
@@ -189,7 +189,7 @@ def create_new_project(window, name, author):
     project = Project(world, author, obengine.version_string())
     world.add_element(obengine.gfx.element3d.SkyboxElement(window))
     world.add_element(obengine.gfx.element3d.LightElement('ambient_light', window, 'ambient'))
-    world.add_element(obengine.gfx.element3d.LightElement('sun_light', window, rotation = obengine.math.EulerAngle(45, 45, 0)))
+    world.add_element(obengine.gfx.element3d.LightElement('sun_light', window, rotation = obengine.math.EulerAngle(45, -45, 0)))
 
     obengine.vfs.open('/bloxworks-registry/project', 'w').write(project)
 
