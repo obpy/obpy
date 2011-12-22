97595b7ed34bfe72aa2315474db0966b7e49135f
openblocks <openblocks@localhost>
2011-12-21 22:40:52 -0600

    Forgot to commit enhancements to obplugin.core_elements.tool

--- src/plugins/core-elements/core_elements/tool.py
+++ src/plugins/core-elements/core_elements/tool.py
@@ -28,8 +28,14 @@ __date__ = "Dec 19, 2011 3:58:46 PM"
 
 import obengine.interface
 import obengine.event
+import obengine.async
+import obengine.vfs
+import obengine.plugin
 import obengine.element
 import obengine.gfx.player
+
+
+TOOL_DIR = 'tools'
 
 
 class ToolElement(obengine.element.Element):
@@ -46,17 +52,22 @@ class ToolElement(obengine.element.Element):
 
         self.on_equipped = obengine.event.Event()
         self.on_unequipped = obengine.event.Event()
+        self._equipped = False
 
         self.on_parent_changed += self._check_for_equip
 
         self._script.parent = self
         self._script.execute()
 
+        if TOOL_DIR not in obengine.vfs.listdir('/'):
+            obengine.vfs.mount('/' + TOOL_DIR, obengine.fs.MemoryFS)
+
     def equip(self):
 
         assert self._player is not None
 
         self._model.show()
+        self._equipped = True
         self.on_equipped()
 
     def unequip(self):
@@ -64,13 +75,50 @@ class ToolElement(obengine.element.Element):
         assert self._player is not None
 
         self._model.hide()
+        self._equipped = False
         self.on_unequipped()
+
+    @property
+    def equipped(self):
+        return self._equipped
 
     def _check_for_equip(self, new_parent):
 
         if obengine.interface.implements(new_parent, obengine.gfx.player.PlayerController):
 
             self._player = new_parent
+
             if self._model is not None:
                 # TODO: Replace use of private attributes here with something better!
                 self._model.parent = self._player._view._model
+
+        else:
+
+            self._model.parent = self._model.window
+            self._player = None
+
+
+class XmlToolParser(obengine.element.XmlElementParser):
+
+    tag = 'tool'
+
+    def parse(tag):
+
+        name = tag.attrib['name']
+
+        script_path = obengine.vfs.getsyspath('/data/%s/%s/tool.lua' % (TOOL_DIR, name))
+        element_factory = obengine.elementfactory.ElementFactory()
+        tool_script = element_factory.make('tool-script', script_path)
+
+        import obplugin.core.graphics
+
+        mesh_path = tag.attrib.get('mesh', None)
+        tool_mesh = None
+        if mesh_path is not None:
+            mesh_path = obengine.vfs.getsyspath('/data/%s/%s/%s' % (TOOL_DIR, name, mesh_path))
+            tool_mesh = obplugin.core.graphics.Model(mesh_path, element_factory.window)
+            tool_mesh.load()
+            obengine.async.wait_on(tool_mesh.load_okay, element_factory.window.scheduler)
+
+        tool_element = ToolElement(name, tool_script, tool_mesh)
+        return tool_element
