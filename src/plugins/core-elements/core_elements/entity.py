4e6b9f58eecf0ebd4215b7d9999fa12c3daac19d
openblocks <openblocks@localhost>
2012-03-22 15:18:04 -0500

    Added full respawn support to EntityElement; commited SpawnElement I've been working on (not finished)

--- src/plugins/core-elements/core_elements/entity.py
+++ src/plugins/core-elements/core_elements/entity.py
@@ -26,8 +26,10 @@ __author__ = "openblocks"
 __date__ = "Dec 27, 2011 9:34:19 PM"
 
 
+import obengine.log
 import obengine.event
 import obengine.datatypes
+import obengine.vfs
 import obengine.element
 import obengine.math
 
@@ -36,9 +38,9 @@ import obplugin.core.physics
 
 class EntityElement(obengine.element.Element):
 
-    def __init__(self, name, max_health, respawn = True, respawn_filter = None, parent = None):
+    def __init__(self, name, position = None, max_health = 100, respawn = True, respawn_filter = None):
 
-        obengine.element.Element.__init__(self, name, parent)
+        obengine.element.Element.__init__(self, name)
 
         self._max_health = max_health
         self._health = health
@@ -48,9 +50,18 @@ class EntityElement(obengine.element.Element):
 
         self._velocity = obengine.math.Vector()
         self.on_falling = obengine.event.Event()
+        self.on_position_changed = obengine.event.Event()
+
+        if position is None:
+            self._position = obengine.math.Vector()
+
+        else:
+            self._position = position
 
         self._respawn = respawn
         self._respawn_filter = respawn_filter
+        if self._respawn is True:
+            self.on_died += self._auto_respawn
 
     def take_damage(self, damage):
 
@@ -58,6 +69,17 @@ class EntityElement(obengine.element.Element):
 
         if self._health == 0:
             self.on_died()
+
+    @obengine.datatypes.nested_property
+    def position():
+
+        def fget(self):
+            return self._position
+
+        def fset(self, new_position):
+
+            self._position = new_position
+            self.on_position_changed(new_position)
 
     @obengine.datatypes.nested_property
     def max_health():
@@ -92,3 +114,25 @@ class EntityElement(obengine.element.Element):
 
     def take_damage(self, damage):
         self.health -= damage
+
+    def _auto_respawn(self):
+
+        try:
+            spawn_points = obengine.vfs.listdir('/spawn-points')
+
+        except obengine.vfs.NonExistentMountError:
+
+            obengine.log.warn('Tried to respawn entity %s, but no spawn points exist' % self.name)
+            return
+
+        else:
+            for spawn_point_file in spawn_points:
+
+                spawn_point = obengine.vfs.open('/spawn-points/%s' % spawn_point_file).read()
+
+                if self._respawn_filter is not None:
+                    if self._respawn_filter(spawn_point) is False:
+                        continue
+
+                self.health = self.max_health
+                self.position = spawn_point.position
